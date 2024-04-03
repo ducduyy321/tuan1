f1f7d468365c0a7a361fc88cada0a0f65e924e28
truong duc duy <ducduy@MacBook-Air-cua-truong.local>
2024-04-03 09:19:17 +0700

    day bai tap oop

---
+++ src/buoi12.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class buoi12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double weight, height, Bmi;
+
+        System.out.println("FAEFAF");
+        weight = sc.nextDouble();
+        System.out.println("FASFA");
+        height = sc.nextDouble();
+
+        Bmi = weight + height;
+        System.out.println(Bmi);
+    }
+}
